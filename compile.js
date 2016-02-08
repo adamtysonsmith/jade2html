e3c3adda9e7ae1279a3e008c05a4929f80908319
Adam Smith <atsmith227@gmail.com>
2016-02-07 21:20:25 -0700

    Update compile.js

--- compile.js
+++ compile.js
@@ -54,51 +54,5 @@ module.exports = {
   error: compileError,
   done: compileDone
 }
-
-// Possible solutions to stuff
-// https://github.com/jprichardson/node-fs-extra - Output to directories
-// http://webapplog.com/jade/ - tutorial this code is based on
-
-// Example website structure
-// -----------------------------
-// index.html
-// about.html
-// work.html
-// contact.html
-// clients.html
-// work/
-//   work-1.html
-//   work-2.html
-// clients/
-//   client-1.html
-//   client-2.html
-
-// Data Management
-// CSV > JSON > Compiled Jade
-
-
-
-/*
-
-    App Requirements
-    - Develop locally with dynamic templating by running $npm run dev
-    - Compile the dynamic templates to HTML by running $npm run compile
-    
-    The current API
-    - Use Compile functions to build a compilation
-    - Use the RouteHelper function to pass data to children routes
-    - Use :filename param
-    - 
-    
-*/
-
-// Concerns
-// how do you distinguish main level from detail level pages?
-// what if you had to build out a complex tree?
-
-// Solution
-// Use several composable functions to build a compilation ***
-// could use config file.. (maybe consider it)
-
 // Example
 // Make x directory then compile these templates, then make y directory for these templates, then x, etc
